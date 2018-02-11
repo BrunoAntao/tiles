2c55fad553f0e4145f8df642db61c9da85ef603e
Bruno Antão <brunomantao@gmail.com>
2018-02-11 14:47:58 +0000

    added simple textbox change typings AGAIN

--- src/resourceEditor/game.ts
+++ src/resourceEditor/game.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/phaser-input/phaser-input.d.ts" /> 
 import { socket } from "./boot";
 
 export class gameState extends Phaser.State {
@@ -5,6 +6,7 @@ export class gameState extends Phaser.State {
     public ctrls;
     public groundPoint = { x: 0, y: 0 };
     public origDragPoint = null;
+    public game: PhaserInput.InputFieldGame;
 
     preload() {
 
@@ -25,7 +27,56 @@ export class gameState extends Phaser.State {
         this.game.renderer.renderSession.roundPixels = true;
         Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
         this.game.canvas.oncontextmenu = function (e) { e.preventDefault(); };
-        this.stage.backgroundColor = "#212121";
+        this.stage.backgroundColor = "#000000";
+
+        this.game.add.plugin(new PhaserInput.Plugin(this.game, this.game.plugins));
+
+        var type = this.game.add.inputField(
+            this.game.width / 3 * 2,
+            this.game.height / 10 * 2, {
+                font: '18px Arial',
+                fill: '#212121',
+                fontWeight: 'bold',
+                width: 150,
+                padding: 8,
+                borderWidth: 1,
+                borderColor: '#000',
+                borderRadius: 6,
+                placeHolder: 'Type',
+                type: PhaserInput.InputType.text
+            });
+
+        var quantity = this.game.add.inputField(
+            this.game.width / 3 * 2,
+            this.game.height / 10 * 3, {
+                font: '18px Arial',
+                fill: '#212121',
+                fontWeight: 'bold',
+                width: 150,
+                padding: 8,
+                borderWidth: 1,
+                borderColor: '#000',
+                borderRadius: 6,
+                placeHolder: 'Quantity',
+                type: PhaserInput.InputType.text
+            });
+
+        var color = this.game.add.inputField(
+            this.game.width / 3 * 2,
+            this.game.height / 10 * 4, {
+                font: '18px Arial',
+                fill: '#212121',
+                fontWeight: 'bold',
+                width: 150,
+                padding: 8,
+                borderWidth: 1,
+                borderColor: '#000',
+                borderRadius: 6,
+                placeHolder: 'Color',
+                type: PhaserInput.InputType.text
+        });
+
+        
 
     }
 
